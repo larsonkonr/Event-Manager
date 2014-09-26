5132123ab8a6c1843f686404fce4a534e49bc9bf
Konr Larson <larsonkonr@gmail.com>
2014-09-26 15:46:20 -0600

    required csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,15 @@
-puts "EventManager Initialized!"
+File.expand_path '../', __FILE__  # => "/Users/larsonkonr/turing/week4/event_manager/lib"
+puts "EventManager Initialized!"  # => nil
+
+lines = File.readlines "event_attendees.csv"  # ~> Errno::ENOENT: No such file or directory @ rb_sysopen - event_attendees.csv
+lines.each do |line|
+  puts line
+end
+
+# >> EventManager Initialized!
+
+# ~> Errno::ENOENT
+# ~> No such file or directory @ rb_sysopen - event_attendees.csv
+# ~>
+# ~> /Users/larsonkonr/turing/week4/event_manager/lib/event_manager.rb:4:in `readlines'
+# ~> /Users/larsonkonr/turing/week4/event_manager/lib/event_manager.rb:4:in `<main>'
