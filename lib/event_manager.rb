c603e3c72784b56601e0f8a01e1b242097b00ebf
Viki Harrod <victoria.harrod@gmail.com>
2014-09-26 16:36:48 -0600

    removed header using each_with_index instead of ignoring header text

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,8 @@ File.expand_path '../', __FILE__
 puts "EventManager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+lines.each_with_index do |line, index|
+  next if  index == 0
   columns = line.split(",")
   name = columns[2]
   puts name
