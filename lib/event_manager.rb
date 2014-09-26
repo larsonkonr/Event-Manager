2c8c49784d117ad53a9a3e6c75d61a04721bf75a
Viki Harrod <victoria.harrod@gmail.com>
2014-09-26 15:50:45 -0600

    split lines on commas, pulled out first names only

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,9 @@
-File.expand_path '../', __FILE__  # => "/Users/larsonkonr/turing/week4/event_manager/lib"
-puts "EventManager Initialized!"  # => nil
+File.expand_path '../', __FILE__
+puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"  # ~> Errno::ENOENT: No such file or directory @ rb_sysopen - event_attendees.csv
+lines = File.readlines "event_attendees.csv"
 lines.each do |line|
-  puts line
+  columns = line.split(",")
+  name = columns[2]
+  puts name
 end
-
-# >> EventManager Initialized!
-
-# ~> Errno::ENOENT
-# ~> No such file or directory @ rb_sysopen - event_attendees.csv
-# ~>
-# ~> /Users/larsonkonr/turing/week4/event_manager/lib/event_manager.rb:4:in `readlines'
-# ~> /Users/larsonkonr/turing/week4/event_manager/lib/event_manager.rb:4:in `<main>'
