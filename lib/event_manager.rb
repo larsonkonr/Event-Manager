56dcc93ea5e3810298b1833b85f0e193c8de7b0e
Viki Harrod <victoria.harrod@gmail.com>
2014-09-27 22:51:08 -0600

    added legislators_by_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,28 @@
-  require 'csv'
-  puts "EventManager Initialized!"
+require 'csv'
+require 'sunlight/congress'
 
-  def clean_zipcode(zipcode)
-    zipcode.to_s.rjust(5, "0")[0..4]
-  end
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-  contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
 
-  contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 
-    puts "#{name} #{zipcode}"
-  end
+  legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end.join(", ")
+end
+puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = get_legislators(zipcode)
+  puts "#{name} #{zipcode} #{legislators}"
+end
